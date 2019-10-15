f0b114fdd2e4ce4443dcdc864d70f6a90c1b232d
LaurentBenazet <benazetlaurent@gmail.com>
2019-10-15 18:56:47 +0200

    Spring initialisation, added gitignore

---
+++ project1920/src/test/java/org/polytech/al/project1920/ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.polytech.al.project1920;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
