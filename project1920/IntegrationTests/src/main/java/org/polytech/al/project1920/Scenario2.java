e3080223dc6c0c451590d47b2c5a238197f754d6
Baptiste Frere <baptiste.frere@etu.unice.fr>
2019-10-25 12:11:36 +0200

    Added basic Scenarios, not implemented yet

--- project1920/IntegrationTests/src/main/java/org/polytech/al/project1920/Scenario2.java
+++ project1920/IntegrationTests/src/main/java/org/polytech/al/project1920/Scenario2.java
@@ -1,46 +1,38 @@
 package org.polytech.al.project1920;
 
-import java.awt.*;
+class Scenario2 {
 
-class Scenario1 {
-
-    Scenario1(){ }
+    Scenario2(){ }
 
     /*
-    * Marcel se connecte à son compte utilisateur (user account) grâce au module d’authentification (authentication).
-    * Le module de recommandations (recommandations) compare le profil généré par le profileur (profiling)
-    * à partir des données recueillies sur Marcel avec les produits du catalogue (catalog).
-    * Il remarque que Marcel aura bientôt 18 ans et ne possède pas de compte jeune.
-    * Il lui affiche alors une proposition pour créer un compte jeune, adapté à sa situation.
+    * José et Killian possèdent tous deux un compte (“bank account”) dans notre banque.
+    * José effectue un virement (“transfer”) à destination de Killian, d’une valeur de 10 000 euros.
+    * Killian possède alors 12 300 euros sur son compte courant : dans les prochains jours, le système de recommandation (“recommandations”)
+    * comparera le profil généré par le profileur (“profiling”) pour Killian avec le catalogue des produits (“catalog”),
+    * et lui proposera de créer un compte (“bank account”) avec un taux plus avantageux.
     */
     void play(){
         System.out.println(Color.ANSI_CYAN+"--------------------------------------------------------------------");
-        System.out.println("                             Scenario 1");
+        System.out.println("                             Scenario 2");
         System.out.println("--------------------------------------------------------------------"+Color.ANSI_RESET);
-        //create account Marcel
+        //create account Jose + Killian
+        //mock add money
         System.out.println();
         System.out.println("Marcel se connecte a son compte utilisateur.");
-        //requete login
         System.out.println("Requete blabla");
         System.out.println("reponse blabla");
         System.out.println();
-        //profiling action
         System.out.println("Le profileur à généré un profil pour Marcel a partir de ses données.");
-        System.out.println("Le module de recommandations récupère le profil généré par le profileur.");
-        //requete get profile
         System.out.println("Requete blabla");
         System.out.println("reponse blabla");
         System.out.println();
         System.out.println("Le module de recommandations récupère les données du catalogue et le profil généré par le profileur.");
-        //requete get catalog
         System.out.println("Requete blabla");
         System.out.println("reponse blabla");
         System.out.println();
-        //action recommendation
         System.out.println("Il remarque que Marcel aura bientot 18 ans et ne possède pas de compte jeune.");
         System.out.println("Il lui affiche alors une proposition pour créer un compte jeune.");
         System.out.println("Proposition blabla");
-        System.out.println();
 
     }
 }
