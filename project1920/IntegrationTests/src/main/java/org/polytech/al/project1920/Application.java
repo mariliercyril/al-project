b9986055b1a4ec9cf9fbb59aea4427533a2d9dfb
Noé Menard <noe.menard@etu.univ-cotedazur.fr>
2019-11-12 20:41:14 +0100

    Properly exit app in intergrationTests

--- project1920/IntegrationTests/src/main/java/org/polytech/al/project1920/Application.java
+++ project1920/IntegrationTests/src/main/java/org/polytech/al/project1920/Application.java
@@ -2,16 +2,24 @@ package org.polytech.al.project1920;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(Application.class, args);
+
+        ApplicationContext context;
+
 
         String uri = "http://192.168.99.100";
         Scenario1 scenario1 = new Scenario1(uri);
@@ -20,5 +28,9 @@ public class Application {
         Scenario2 scenario2 = new Scenario2(uri);
         scenario2.play();
 
-        }
+        System.out.println("The scenarios are over, the app will now shutdown");
+        Thread.sleep(1000);
+
+        System.exit(0);
+    }
 }
